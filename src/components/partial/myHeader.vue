c9b18422e1ca96f8c10771417ec0ec284916b875
Jahurul Islam <43181519+jahurulislamjl@users.noreply.github.com>
2020-02-29 23:12:08 +0600

    home page

---
+++ src/components/partial/myHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="header-section">
+        <header class="header">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <a href="" class="navbar-brand">brand</a>
+
+                <div class="navbar-link-item">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <button class="nav-link"  @click="$root.scrollToElement('about')">
+                                about
+                            </button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link"  @click="$root.scrollToElement('about')">
+                                skills
+                            </button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link"  @click="$root.scrollToElement('about')">
+                                portfolio
+                            </button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link"  @click="$root.scrollToElement('about')">
+                                experience
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="home-inner">
+            <div class="dark-overlay">
+
+            </div>
+        </div>
+
+        <section class="about">
+            <div class="container">
+                <div class="row">
+                    <div class="about-me">
+
+                        <div class="profile">
+                            <div class="row">
+
+                                <div class="col-md-4 profile-picture p-5">
+                                    <img src="../assets/images/hur.jpg" alt="" width="100%">
+                                </div>
+
+                                <div class="col-md-8 biodata p-5">
+                                    <div class="intro">
+                                        <p>HELLO</p>
+                                    </div>
+                                    <div class="name">
+                                        <p>I'm Md. Jahurul Islam</p>
+                                        <p>Software Engineer</p>
+                                    </div>
+
+                                    <div class="bio-details">
+                                        <div class="bio-label">
+                                            <div class="bio-title">
+                                                <p>AGE</p>
+                                            </div>
+                                            <div class="bio-description">
+                                                <p>25</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="bio-label">
+                                            <div class="bio-title">
+                                                <p>ADDRESS</p>
+                                            </div>
+                                            <div class="bio-description">
+                                                <p>JASHORE</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="bio-label">
+                                            <div class="bio-title">
+                                                <p>EMAIL</p>
+                                            </div>
+                                            <div class="bio-description">
+                                                <p>jahurul@gmail.com</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="bio-label">
+                                            <div class="bio-title">
+                                                <p>PHONE</p>
+                                            </div>
+                                            <div class="bio-description">
+                                                <p>017XXXXXXXXXX</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="social-media p-5">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="skills py-5">
+            <div class="container">
+                <div class="row">
+                    skills
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
